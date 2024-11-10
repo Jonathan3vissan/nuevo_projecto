b9e1ac6a250d91e56ed812a43e45374eca397944
Jonnthan3visan <joptrabajo@hotmail.com>
2024-11-09 21:10:36 -0300

    registro.html
    funciona correctamente

---
+++ clases/Usuario.js
@@ -0,0 +1,32 @@
+class Usuario {
+    #nombre = "def nombre";  
+    #mail = "def mail";      
+    #telefono = "def telefono";  
+    constructor(nombre = '', mail = '', telefono = '') {
+        this.#nombre = nombre;
+        this.#mail = mail;
+        this.#telefono = telefono;
+    }
+    /**
+     * Obtiene el nombre del usuario.
+     * @returns {string} El nombre del usuario.
+     */
+    getNombre() {
+        return this.#nombre;
+    }
+    /**
+     * Obtiene el email del usuario.
+     * @returns {string} El email del usuario.
+     */
+    getMail() {
+        return this.#mail;
+    }
+    /**
+     * Obtiene el teléfono del usuario.
+     * @returns {string} El teléfono del usuario.
+     */
+    getTelefono() {
+        return this.#telefono;
+    }
+}
+export default Usuario;
